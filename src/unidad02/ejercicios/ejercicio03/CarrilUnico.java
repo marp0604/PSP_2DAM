1a0055c8560ef4e3bc5b0176e9ffd9b923ad7b06
byelm <marp0604.programador@gmail.com>
2025-10-26 11:46:04 +0100

    Commit Ejercicio03 -> Enum e interface

---
+++ src/unidad02/ejercicios/ejercicio03/CarrilUnico.java
@@ -0,0 +1,20 @@
+package unidad02.ejercicios.ejercicio03;
+
+
+/**
+ * @author Miguel Angel Ramirez
+ */
+public interface CarrilUnico {
+    /**
+     * Intenta entrar en el carril. Si está ocupado, el hilo debe esperar.
+     * @param direccion La dirección del vehículo que entra
+     * @throws InterruptedException Si el hilo se interrumpe mientras espera
+     */
+    void entrar(Direccion direccion) throws InterruptedException;
+
+    /**
+     * Sale del carril. Debe notificar a los hilos que espera
+     * @param direccion La dirección del vehículo que sale
+     */
+    void sali(Direccion direccion);
+}
---
+++ src/unidad02/ejercicios/ejercicio03/Direccion.java
@@ -0,0 +1,13 @@
+package unidad02.ejercicios.ejercicio03;
+
+
+/**
+ * Enum que define las direcciones que tienen los vehiculos.
+ *
+ * @author Miguel Angel Ramirez
+ */
+public enum Direccion {
+    IZDA,
+    DCHA,
+    NINGUNA
+}
