1438c2b5f436392dd3687358b6b05b1aa858b49d
Miguel Angel <marp0604.programador@gmail.com>
2025-10-30 11:14:40 +0100

    Commit Ejercicio 04 -> Contenedor.java y VehiculoDescarga.java

---
+++ src/unidad02/ejercicios/ejercicio04/Contenedor.java
@@ -0,0 +1,32 @@
+package unidad02.ejercicios.ejercicio04;
+
+
+/**
+ * @author Miguel Angel Ramirez
+ */
+public class Contenedor {
+    private final int ID;
+    private Producto contenido;
+
+    public Contenedor(int id, Producto contenidoInicial){
+        this.ID = id;
+        this.contenido = contenidoInicial;
+    }
+
+    public void llenar(Producto producto){
+        this.contenido = contenido;
+    }
+
+    public void vaciar(){
+        this.contenido = Producto.NINGUNO;
+    }
+
+    public boolean estaVacio(){
+        return this.contenido == Producto.NINGUNO;
+    }
+
+    @Override
+    public String toString() {
+        return "Contenedor <" + ID + "> en estos momentos contiene <" + contenido + ">";
+    }
+}
---
+++ src/unidad02/ejercicios/ejercicio04/VehiculoDescarga.java
@@ -0,0 +1,28 @@
+package unidad02.ejercicios.ejercicio04;
+
+
+import java.util.Random;
+import java.util.concurrent.Exchanger;
+
+/**
+ * @author Miguel Angel Ramirez
+ */
+public class VehiculoDescarga implements Runnable{
+    private Exchanger<Contenedor> puntoIntercambio;
+    private Contenedor miContenedor;
+    private int numCiclos;
+
+    public VehiculoDescarga(Exchanger<Contenedor> puntoIntercambio, Contenedor contenedorInicial, int numCiclos){
+        this.puntoIntercambio = puntoIntercambio;
+        this.miContenedor = contenedorInicial;
+        this.numCiclos = numCiclos;
+    }
+
+    @Override
+    public void run() {
+        try{
+            for (int i = 1; i <= this.numCiclos; i++){
+            }
+        } catch ()
+    }
+}
