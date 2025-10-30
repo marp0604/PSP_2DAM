5e351fd0056908fc2c0616923d31ae708d61463c
Miguel Angel <marp0604.programador@gmail.com>
2025-10-30 10:37:26 +0100

    Commit Ejercicio 04 -> Enum Producto.java

---
+++ src/unidad02/ejercicios/ejercicio04/Producto.java
@@ -0,0 +1,14 @@
+package unidad02.ejercicios.ejercicio04;
+
+
+/**
+ * @author Miguel Angel Ramirez
+ */
+public enum Producto {
+    NINGUNO,
+    SERRIN,
+    NARANJAS,
+    CANDADOS,
+    AGUA,
+    MADERA
+}
